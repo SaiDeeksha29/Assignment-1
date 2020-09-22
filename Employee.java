2fc2a2c1997ec2e6428cd1518db0b4b5cda0d951
SaiDeeksha29 <saideeksha29@gmail.com>
2020-09-22 20:12:42 +0530

    Welcome message

--- Employee.java
+++ Employee.java
@@ -14,7 +14,7 @@ public class Employee {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("Welcome to Employee");
+        System.out.println("Welcome to Employee Wage Computation");
 	}
 
 }
