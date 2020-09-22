edcc1361369b0bc9aa5211e7fa683a7f23e8be47
SaiDeeksha29 <saideeksha29@gmail.com>
2020-09-22 23:30:01 +0530

    Final commit

--- Employee.java
+++ Employee.java
@@ -18,27 +18,22 @@ public class Employee {
         int IS_PART_TIME=2;
         int IS_FULL_TIME=1;
         int empWage=0;
-        int empHour=0;
+        int MAX_WORKING_HOUR=100;
         int EMP_WAGE_PER_HOUR=20;
         int empCheck=(int)(Math.floor(Math.random()*10)%3);
-        int workingDays=20;
-        switch(empCheck) {
-        case 1:
-        	empCheck=IS_FULL_TIME;
+        int MAX_WORKING_DAYS=20;
+        if(empCheck==IS_FULL_TIME) {
         	System.out.println("Employee is present-FULL TIME");
-        	empHour=8;
-        	break;
-        case 2:
-        	empCheck=IS_PART_TIME;
-        	System.out.println("Employee is present-PART TIME");
-        	empHour=4;
-        	break;
-        default:
-        	System.out.println("Employee is absent");
-            break;
+        	empWage=MAX_WORKING_HOUR*EMP_WAGE_PER_HOUR;
         }
-        empWage=empHour*EMP_WAGE_PER_HOUR*workingDays;
-        System.out.println("Employee Wage: "+empWage);
+        else if(empCheck==IS_PART_TIME) {
+        	System.out.println("Employee is present-PART TIME");
+        	empWage=4*EMP_WAGE_PER_HOUR*MAX_WORKING_DAYS;
+        }
+        else {
+        	System.out.println("Employee is absent");
+        }
+        System.out.println(empWage);
 	}
 
 }
