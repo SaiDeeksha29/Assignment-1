bbf215b26dc176b9b990c59dc4eec788f5a39e2b
SaiDeeksha29 <saideeksha29@gmail.com>
2020-09-22 23:24:15 +0530

    Switch Case

--- Employee.java
+++ Employee.java
@@ -16,15 +16,23 @@ public class Employee {
 		// TODO Auto-generated method stub
         System.out.println("Welcome to Employee Wage Computation");
         int IS_PART_TIME=2;
-        int EMP_WAGE_PER_HOUR=20;
-        int empHour=0;
+        int IS_FULL_TIME=1;
         int empWage=0;
-        double empCheck=Math.floor(Math.random()*10)%3;
-        if(empCheck==IS_PART_TIME) {
+        int empHour=0;
+        int EMP_WAGE_PER_HOUR=20;
+        int empCheck=(int)(Math.floor(Math.random()*10)%3);
+        switch(empCheck) {
+        case 1:
+        	empCheck=IS_FULL_TIME;
+        	empHour=8;
+        	break;
+        case 2:
+        	empCheck=IS_PART_TIME;
         	empHour=4;
-        }
-        else {
-        	System.out.println("Employee is absent");
+        	break;
+        default:
+            empHour=0;
+            break;
         }
         empWage=empHour*EMP_WAGE_PER_HOUR;
         System.out.println("Employee Wage: "+empWage);
