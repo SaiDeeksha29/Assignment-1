ca5ef68d7228f313a21c60f85a10ef4164377a9f
SaiDeeksha29 <saideeksha29@gmail.com>
2020-09-22 19:04:16 +0530

    Welcome message

---
+++ Employee.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package employee;
+
+/**
+ * @author saideeksha
+ *
+ */
+public class Employee {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Welcome to Employee");
+	}
+
+}
