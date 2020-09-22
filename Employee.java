2a5fa5874c3c687279be1a61b6900a91e7c92bab
SaiDeeksha29 <saideeksha29@gmail.com>
2020-09-22 23:21:29 +0530

    Part Time Employee Added

--- Employee.java
+++ Employee.java
@@ -15,14 +15,13 @@ public class Employee {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Welcome to Employee Wage Computation");
+        int IS_PART_TIME=2;
         int EMP_WAGE_PER_HOUR=20;
         int empHour=0;
         int empWage=0;
-        int IS_FULL_TIME=1;
-        double empCheck=Math.floor(Math.random()*10)%2;
-        if(empCheck==IS_FULL_TIME) {
-        	System.out.println("Employee is present");
-        	empHour=8;
+        double empCheck=Math.floor(Math.random()*10)%3;
+        if(empCheck==IS_PART_TIME) {
+        	empHour=4;
         }
         else {
         	System.out.println("Employee is absent");
