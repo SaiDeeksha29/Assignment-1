b610347b4c723ca445d0f7c86a2b82fb7c58aebe
SaiDeeksha29 <saideeksha29@gmail.com>
2020-09-25 09:51:49 +0530

    Employee wage depending on conditions

--- Employee.java
+++ Employee.java
@@ -14,37 +14,63 @@ public class Employee {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("Welcome to Employee Wage Computation");
-        int IS_PART_TIME=2;
-        int IS_FULL_TIME=1;
-        int empWage=0;
-        int empHour=0;
-        int MAX_WORKING_HOUR=100;
-        int EMP_WAGE_PER_HOUR=20;
-        int empCheck=(int)(Math.floor(Math.random()*10)%3);
-        int MAX_WORKING_DAYS=20;
-        switch(empCheck) {
-        case 1:
-        	empCheck=IS_FULL_TIME;
-        	System.out.println("Employee Present - FULL TIME");
-        	empHour=8;
-        	break;
-        case 2:
-        	empCheck=IS_PART_TIME;
-        	System.out.println("Employee Present - PART TIME");
-        	empHour=4;
-        	break;
-        default:
-            empHour=0;
-            break;
-        }
-        if(MAX_WORKING_HOUR/empHour<MAX_WORKING_DAYS && empHour!=0) {
-        	empWage=MAX_WORKING_HOUR*EMP_WAGE_PER_HOUR;
-        }
-        else if(MAX_WORKING_HOUR/empHour>MAX_WORKING_DAYS && empHour!=0) {
-        	empWage=4*EMP_WAGE_PER_HOUR*MAX_WORKING_DAYS;
-        }
-        System.out.println("Employee Wage : "+empWage);
-	}
 
+		// variables
+		int IS_PART_TIME = 2;
+		int IS_FULL_TIME = 1;
+		int empWage = 0;
+		int empHour = 0;
+		int dayCount = 0;
+		int hoursWorked = 0;
+		int totalWage = 0;
+		int MAX_WORKING_HOUR = 100;
+		int EMP_WAGE_PER_HOUR = 20;
+		int MAX_WORKING_DAYS = 20;
+
+		// Welcome Message
+		System.out.println("Welcome to Employee Wage Computation");
+
+		// Computation
+		int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+
+		switch (empCheck) {
+		case 1:
+			empCheck = IS_FULL_TIME;
+			System.out.println("Employee Present - FULL TIME");
+			empHour = 8;
+			break;
+		case 2:
+			empCheck = IS_PART_TIME;
+			System.out.println("Employee Present - PART TIME");
+			empHour = 4;
+			break;
+		default:
+			break;
+		}
+
+		// Calculation of Daily Employee Wage
+		empWage = empHour * EMP_WAGE_PER_HOUR;
+
+		// Tabular Display of Employee Details for Maximum Monthly Hours or Days
+		System.out.println("Day\tHours Worked\tTotal Wage");
+		while ((hoursWorked + empHour) <= MAX_WORKING_HOUR && dayCount < MAX_WORKING_DAYS) {
+			dayCount++;
+			hoursWorked += empHour;
+			totalWage += empWage;
+			System.out.println(" " + dayCount + "  \t" + hoursWorked + "  \t\t" + totalWage);
+
+		}
+
+		// Calculations of Employee Wage for Hours left out of Maximum Hours
+		if (hoursWorked < MAX_WORKING_HOUR && dayCount < MAX_WORKING_DAYS) {
+			dayCount++;
+			totalWage += (MAX_WORKING_HOUR - hoursWorked) * EMP_WAGE_PER_HOUR;
+			hoursWorked = hoursWorked + (MAX_WORKING_HOUR - hoursWorked);
+			System.out.println(" " + dayCount + "  \t" + hoursWorked + "  \t\t" + totalWage);
+		}
+
+		// Display of Employee Total Wage
+		System.out.println("The Employee Total Wage for Maximum Hours or Days : " + totalWage);
+
+	}
 }
